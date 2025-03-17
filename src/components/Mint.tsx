88aa4a501479d45be3dd7c0cfd0384575573d3de
Nikita Kolmogorov <backmeupplz@gmail.com>
2025-03-16 19:20:07 -0700

    Refactor Mint component button layout for improved structure and readability

--- src/components/Mint.tsx
+++ src/components/Mint.tsx
@@ -55,56 +55,58 @@ export default function Mint({
           It will get you <b>{Math.floor(amount * 10500)} $merv</b>
         </p>
       </fieldset>
-      <button
-        className="btn btn-primary"
-        disabled={!amount}
-        onClick={async () => {
-          try {
-            if (!connectors[0]) {
-              toast.error('No connector found')
-              return
-            }
-            if (!account.isConnected) {
-              const connectResult = await connectAsync({
-                connector: connectors[0],
-                chainId: base.id,
-              })
-
-              if (!connectResult.accounts.length) {
-                toast.error('Unable to connect: no addresses')
+      <div className="flex flex-row gap-2">
+        <button
+          className="btn btn-primary"
+          disabled={!amount}
+          onClick={async () => {
+            try {
+              if (!connectors[0]) {
+                toast.error('No connector found')
                 return
               }
+              if (!account.isConnected) {
+                const connectResult = await connectAsync({
+                  connector: connectors[0],
+                  chainId: base.id,
+                })
+
+                if (!connectResult.accounts.length) {
+                  toast.error('Unable to connect: no addresses')
+                  return
+                }
+              }
+              if (chainId !== base.id) {
+                await switchChainAsync({ chainId: base.id })
+              }
+              await writeContractAsync({
+                abi: mervAbi,
+                address: '0xdf65A4E7DAE495Cb42F5A1E9A7f89032542684Ba',
+                functionName: 'mint',
+                value: parseUnits(amount.toString(), 18),
+                chain: base,
+              })
+              toast.success(`Minted ${amount} $merv`, { duration: 5000 })
+            } catch (error) {
+              console.error(error)
+              toast.error(`Failed to mint: ${error.message}`)
             }
-            if (chainId !== base.id) {
-              await switchChainAsync({ chainId: base.id })
-            }
-            await writeContractAsync({
-              abi: mervAbi,
-              address: '0xdf65A4E7DAE495Cb42F5A1E9A7f89032542684Ba',
-              functionName: 'mint',
-              value: parseUnits(amount.toString(), 18),
-              chain: base,
-            })
-            toast.success(`Minted ${amount} $merv`, { duration: 5000 })
-          } catch (error) {
-            console.error(error)
-            toast.error(`Failed to mint: ${error.message}`)
-          }
-        }}
-      >
-        Mint
-      </button>
-      <button
-        className="btn btn-secondary ml-2"
-        onClick={() => {
-          const encodedText = encodeURIComponent("It's merving time!")
-          const encodedUrl = encodeURIComponent('https://merv.fun')
-          const warpcastUrl = `https://warpcast.com/~/compose?text=${encodedText}&embeds[]=${encodedUrl}`
-          return frameSdk.actions.openUrl(warpcastUrl)
-        }}
-      >
-        merv it?
-      </button>
+          }}
+        >
+          Mint
+        </button>
+        <button
+          className="btn btn-secondary ml-2"
+          onClick={() => {
+            const encodedText = encodeURIComponent("It's merving time!")
+            const encodedUrl = encodeURIComponent('https://merv.fun')
+            const warpcastUrl = `https://warpcast.com/~/compose?text=${encodedText}&embeds[]=${encodedUrl}`
+            return frameSdk.actions.openUrl(warpcastUrl)
+          }}
+        >
+          merv it?
+        </button>
+      </div>
     </>
   )
 }
