cdfb0632560375ceb855fa1954c6de5e3789a8f2
Nikita Kolmogorov <backmeupplz@gmail.com>
2025-04-14 14:22:08 -0700

    feat: enhance Main component with claim $merv button and center alignment

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { createFileRoute, Link } from '@tanstack/react-router'
 import useURQLClient from 'hooks/useURQLClient'
 import { useCallback } from 'preact/hooks'
 import { getMeQuery } from 'queries/UserResolver'
+import toast from 'react-hot-toast'
 
 export const Route = createFileRoute('/')({
   component: Main,
@@ -30,13 +31,23 @@ function Main() {
           : Math.round(data?.data?.getMe.unclaimedMerv || 0)}
       </p>
       <p className="font-extrabold text-3xl -mt-20">$merv</p>
-      <div className="flex flex-row flex-wrap gap-2">
+      <div className="flex flex-row flex-wrap gap-2 justify-center">
         <Link to="/accounts">
           <button className="btn btn-primary">Get more $merv for free</button>
         </Link>
         <Link to="/merv">
           <button className="btn btn-primary">Mint $merv</button>
         </Link>
+        {!!data?.data?.getMe.unclaimedMerv && (
+          <button
+            className="btn btn-secondary"
+            onClick={() => {
+              toast.success('Claiming will open when merv officially launches!')
+            }}
+          >
+            Claim $merv
+          </button>
+        )}
       </div>
     </div>
   )
